b24f66bd08e05097700db7fdc7046075f7ca2ecf
Kauã Berman Amorim <kauabamorim@outlook.com>
2021-08-02 20:57:38 -0300

    Get e Set

--- index.js
+++ index.js
@@ -2,24 +2,23 @@ import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = 76920645980;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 12457890980;
 
 const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
 contaCorrenteRicardo.cliente = cliente1;
+contaCorrenteRicardo.agencia = 1001;
 contaCorrenteRicardo.depositar(900);
 
 const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
+conta2.cliente = cliente1;
 conta2.agencia = 102;
 
+let valor = 200;
 contaCorrenteRicardo.transferir(200, conta2);
 
-console.log(contaCorrenteRicardo);
-console.log(conta2);
+console.log(conta2.saldo);
