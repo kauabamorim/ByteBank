7b0716e4e2745d059608b01edc895d8fd7fb8135
Kauã Berman Amorim <kauabamorim@outlook.com>
2021-08-02 11:47:41 -0300

    Usado funcao de Metodos

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente {
     agencia;
     saldo;
+
+    sacar(valor) {
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
@@ -10,12 +19,20 @@ const cliente2 = new Cliente();
 
 cliente1.nome = "Ricardo";
 cliente1.cpf = 76920645980;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
 cliente2.nome = "Alice";
 cliente2.cpf = 12457890980;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1, cliente2);
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.saldo = 100; 
+console.log(contaCorrenteRicardo.saldo);
+
+contaCorrenteRicardo.sacar(50);
+console.log(contaCorrenteRicardo.saldo);
+
+
+console.log(cliente1);
+console.log(cliente2);
