6f78d9f65c7e8b800ed0b81b752a44b3c9b6d0b3
Kauã Berman Amorim <kauabamorim@outlook.com>
2021-08-03 17:50:06 -0300

    Construtores, Organizacao do Codigo

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 76920645980;
+const cliente1 = new Cliente("Ricardo", 76920645980);
+const cliente2 = new Cliente("Alice", 12457890980);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 12457890980;
-
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.cliente = cliente1;
-contaCorrenteRicardo.agencia = 1001;
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
 contaCorrenteRicardo.depositar(900);
 
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente1;
-conta2.agencia = 102;
+const conta2 = new ContaCorrente(102, cliente2);
 
 let valor = 200;
 contaCorrenteRicardo.transferir(200, conta2);
-
-console.log(conta2.saldo);
